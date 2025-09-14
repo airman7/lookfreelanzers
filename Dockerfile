531eb21a56a3d62baf014a8dde601ab0900ae91d
Mayank Vijayvargiya <mayankvijay95@gmail.com>
2025-09-14 20:41:33 +0530

    image size improvement and docker-compose for db+app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM tomcat:8.5.94-jdk17-temurin
-COPY target/LookFreelanzers-4.0.war /usr/local/tomcat/webapps/
+COPY target/lookfreelanzers-4.0.war /usr/local/tomcat/webapps/lookfreelanzers.war
+
+#from first commit
+#FROM maven:3-openjdk-17
+#COPY . .Add commentMore actions
+#ENTRYPOINT ["mvn", "tomcat7:run"]
 
 #CMD ["java", "-jar", "lookfreelanzers.jar"]
 #CMD mvn tomcat7:run  - this command will start shell as an extra process as this is shell format
--- src/main/java/Connection/Conn.java
+++ src/main/java/Connection/Conn.java
@@ -8,11 +8,28 @@ public class Conn {
 	}
 	public static Connection getCon()
 	{
+		String dbHost = System.getenv("DB_HOST");
+		String dbPort = System.getenv("DB_PORT");
+		String dbName = System.getenv("DB_NAME");
+		// String dbUser = System.getenv("DB_USER");
+		// String dbPass = System.getenv("DB_PASS");
+
+		if (dbHost == null) {
+			dbHost = "localhost";
+		}
+		if (dbPort == null) {
+			dbPort = "3306";
+		}
+		if (dbName == null) {
+			dbName = "lookfreelanzers";
+		}
+		
+		String jdbcUrl = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
 	    Connection con=null;
 	    try
 	    {
 	    	Class.forName("com.mysql.cj.jdbc.Driver");
-	    	con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lookfreelanzers?characterEncoding=latin1&useConfigs=maxPerformance",
+	    	con=DriverManager.getConnection(jdbcUrl+"?characterEncoding=latin1&useConfigs=maxPerformance",
 	    			"root","doomsday");
 	    } catch(Exception ex)
 	    {
